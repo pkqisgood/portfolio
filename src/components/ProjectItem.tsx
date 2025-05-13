b1830c699621b8f05110a54798ca6055aff73d7f
pkq <pangkaiqing2000@gmail.com>
2025-05-14 02:22:49 +0800

    update project details layout

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { motion } from "framer-motion";
 import Image from "next/image";
+import { forwardRef } from "react";
 
 interface ProjectItemProps {
     title: string;
@@ -11,46 +11,45 @@ interface ProjectItemProps {
     alias: string;
 }
 
-export default function ProjectItem({ title, description, imageSrc, techStack, alias }: ProjectItemProps) {
-    return (
-        <motion.a
-            href={`/projects/${alias}`}
-            className='pt-7 px-7 md:pt-8 md:px-8 project-item group rounded-2xl hover:shadow-lg transition relative overflow-hidden flex flex-col justify-between'
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true, margin: "-50px 0px" }}
-            whileHover={{ scale: 1.01 }}
-            transition={{
-                duration: 0.6,
-                scale: { type: "spring", stiffness: 300 },
-            }}
-        >
-            <div>
-                <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-                <p className="text-(--secondary-color) mb-4">{description}</p>
+const ProjectItem = forwardRef<HTMLAnchorElement, ProjectItemProps>(
+    ({ title, description, imageSrc, techStack, alias }, ref) => {
+        return (
+            <a
+                ref={ref}
+                href={`/projects/${alias}`}
+                className='pt-7 px-7 md:pt-8 md:px-8 project-item group rounded-2xl hover:shadow-lg transition relative overflow-hidden flex flex-col justify-between'
+            >
+                <div>
+                    <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+                    <p className="text-(--secondary-color) mb-4">{description}</p>
 
-                {techStack && (
-                    <div className="flex flex-wrap gap-2 mb-4">
-                        {techStack.map((tech, index) => (
-                            <span
-                                key={index}
-                                className="bg-[#f0f8fe] px-2 py-1 rounded-xl text-sm"
-                            >
-                                {tech}
-                            </span>
-                        ))}
-                    </div>
-                )}
-            </div>
-            <div className="img-wrapper transition-transform duration-500 ease-in-out group-hover:translate-y-2" >
-                <Image
-                    src={imageSrc}
-                    alt={title}
-                    width={600}
-                    height={400}
-                    className="rounded-xl"
-                />
-            </div>
-        </motion.a>
-    );
-}
+                    {techStack && (
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {techStack.map((tech, index) => (
+                                <span
+                                    key={index}
+                                    className="bg-[#f0f8fe] px-2 py-1 rounded-xl text-sm"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div className="img-wrapper transition-transform duration-500 ease-in-out group-hover:translate-y-2" >
+                    <Image
+                        src={imageSrc}
+                        alt={title}
+                        width={600}
+                        height={400}
+                        className="rounded-xl"
+                    />
+                </div>
+            </a>
+        );
+    }
+)
+
+ProjectItem.displayName = "ProjectItem";
+
+export default ProjectItem;
